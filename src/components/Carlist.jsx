106727618a2851a6290a6a4184704a9dc51f2212
RenneJ <renne.jamsen@gmail.com>
2023-10-28 18:22:10 +0300

    Added delete functionality

--- src/components/Carlist.jsx
+++ src/components/Carlist.jsx
@@ -3,7 +3,7 @@ import {AgGridReact} from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-material.css';
 
-export default function Carlist(){
+export default function Carlist() {
     const [cars, setCars] = useState([]);
     const gridRef = useRef();
 
@@ -13,6 +13,20 @@ export default function Carlist(){
         fetch('http://carrestapi.herokuapp.com/cars')
         .then(response => response.json())
         .then(data => setCars(data._embedded.cars))
+        .catch(err => console.error(err))
+    };
+
+    const buttonRenderer = (row) => {
+        var link = row.data._links.self.href
+        return(
+            <button className='del-btn' onClick={() => handleDelete(link)}>Delete</button>
+        )
+    };
+
+    const handleDelete = (link) => {
+        fetch(link, {method: 'DELETE'})
+        .then(response => fetchData())
+        .catch(err => console.error(err))
     };
 
     const columns = [
@@ -45,14 +59,22 @@ export default function Carlist(){
             headerName: 'Price',
             field: 'price',
             filter: 'true'
+        },
+        {
+            width: '100px',
+            cellRenderer: buttonRenderer
         }
     ]
 
     return(
-        <div className='ag-theme-material' style={{width: '1200px', height: '600px', margin: 'auto', padding: '20px 0'}}>
+        <div className='ag-theme-material' style={{width: '1350px', height: '600px', margin: 'auto', padding: '20px 0'}}>
             <AgGridReact 
                 columnDefs={columns}
                 rowData={cars}
+                ref={gridRef}
+                onGridReady={ params => gridRef.current = params.api }
+                rowSelection='single'
+                animateRows="true"
                 //style={{ width: '100%', height: '600px' }}
             />
         </div>
