55f4562dfd36767ac9b39b24ae30b2390f62e858
RenneJ <renne.jamsen@gmail.com>
2023-10-29 18:05:47 +0200

    Added edit functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import Carlist from './components/Carlist';
 import AppBar from '@mui/material/AppBar';
--- src/components/Carlist.jsx
+++ src/components/Carlist.jsx
@@ -5,6 +5,7 @@ import 'ag-grid-community/styles/ag-theme-material.css';
 import Button from '@mui/material/Button';
 import ConfirmDelete from './ConfirmDelete';
 import AddCar from './AddCar';
+import EditCar from './EditCar';
 
 export default function Carlist() {
     const [cars, setCars] = useState([]);
@@ -18,11 +19,17 @@ export default function Carlist() {
         .catch(err => console.error(err))
     };
 
-    const buttonRenderer = (row) => {
+    const editButton = (row) => {
+        return(
+                <EditCar updateCar={updateCar} car={row.data}/>
+        );
+    };
+
+    const deleteButton = (row) => {
         var link = row.data._links.self.href
         return(
-            <Button variant="contained" size="small" color="error" onClick={() => handleDelete(link)}>Delete</Button>
-        )
+                <Button variant="contained" size="small" color="error" onClick={() => handleDelete(link)}>Delete</Button>
+        );
     };
 
     const handleDelete = (link) => {
@@ -44,6 +51,18 @@ export default function Carlist() {
         .then(response => fetchData())
         .catch(err => console.error(err))
     };
+
+    const updateCar = (car, link) => {
+        fetch(link, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(car)
+    })
+    .then(response => fetchData())
+    .catch(err => console.error(err))
+    }
 
     const columns = [
         {
@@ -95,13 +114,17 @@ export default function Carlist() {
             floatingFilter: true
         },
         {
+            width: 90,
+            cellRenderer: editButton
+        },
+        {
             width: 100,
-            cellRenderer: buttonRenderer
+            cellRenderer: deleteButton
         }
     ]
 
     return(
-        <div className='ag-theme-material' style={{width: '1350px', height: '600px', margin: 'auto', padding: '20px 0'}}>
+        <div className='ag-theme-material' style={{width: '1420px', height: '600px', margin: 'auto', padding: '20px 0'}}>
             <AddCar saveCar={saveCar}/>
             <AgGridReact 
                 columnDefs={columns}
